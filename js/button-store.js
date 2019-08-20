cf9fa42ee86cdc480705062304bbf66707bcb138
Camila Becker <camila.becker01@gmail.com>
2019-08-20 15:43:10 -0300

    Desafio avançado aula 013

--- js/button-store.js
+++ js/button-store.js
@@ -1,8 +1,12 @@
 const $button = window.document.querySelector(".-second");
 console.log($button);
 
+const $cart = window.document.querySelector(".-last");
+console.log($cart);
+
 function clickButton() {
   console.log("Clicou no botão adiciona no carrinho");
+  $cart.innerHTML = "Fui clicado!";
 }
 
 $button.addEventListener("click", clickButton);
