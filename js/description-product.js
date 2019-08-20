4a9942c22cd7bf1c87945dd48741e872b694dc06
Camila Becker <camila.becker01@gmail.com>
2019-08-20 15:31:45 -0300

    Criado arquivo JS e adicionado código que pega o evento de click no coração

---
+++ js/description-product.js
@@ -0,0 +1,17 @@
+/*
+  1 - Ter a janela (browser) na mão;
+  2 - Ter o Html na mão;
+  3 - Pegar o coração;
+  4 - Pegar o clique no coração;
+  5 - No momento que o usuário clicar, nós queremos mostrar um texto AE;
+*/
+
+const $heart = window.document.querySelector(".-heart");
+
+console.log($heart);
+
+function handleClick() {
+  console.log("clicou no coração");
+}
+
+$heart.addEventListener("click", handleClick);
