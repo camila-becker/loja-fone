c346bb0fefe40543457d086e25d1d3486c7edf75
Camila Becker <camila.becker01@gmail.com>
2019-08-20 15:38:33 -0300

    Desafio intermediário aula 013

--- js/button-store.js
+++ js/button-store.js
@@ -1,2 +1,8 @@
 const $button = window.document.querySelector(".-second");
 console.log($button);
+
+function clickButton() {
+  console.log("Clicou no botão adiciona no carrinho");
+}
+
+$button.addEventListener("click", clickButton);
